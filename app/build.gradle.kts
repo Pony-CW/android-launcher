9380a7a847ca18ab9220ad4d7ae709c6d61c3661
无铭 <44076275+Pony-CW@users.noreply.github.com>
2024-08-31 16:43:16 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.geode.launcher211"
+        applicationId = "com.geode.ponycw211"
         minSdk = 23
         targetSdk = 35
         versionCode = 1801
